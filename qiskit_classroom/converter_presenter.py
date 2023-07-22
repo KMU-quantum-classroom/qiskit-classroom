808f882ee008a1e3cc5ebf492411c1dcf7ae3fea
fmowl10 <jinseok1001@hotmail.com>
2023-07-22 13:28:00 +0900

    Fix QComboBox changed event handler

--- qiskit_classroom/converter_presenter.py
+++ qiskit_classroom/converter_presenter.py
@@ -2,8 +2,6 @@
     presenter for converter view
 """
 
-
-import asyncio
 from typing import TYPE_CHECKING
 from .expression_enum import QuantumExpression, expressions
 
@@ -21,6 +19,7 @@ class ConverterPresenter:
         self.view = view
         self.view.set_presenter(self)
         self.model = model
+        self.model.from_expression = QuantumExpression.CIRCUIT
         self.view.from_combo.currentTextChanged.connect(self.on_from_combo_changed)
         self.view.to_combo.currentTextChanged.connect(self.on_to_combo_changed)
         self.view.value_name_text.textChanged.connect(self.on_value_name_changed)
@@ -39,8 +38,10 @@ class ConverterPresenter:
         """
         update from_expression
         """
-        self.model.from_expression = self.view.from_combo.currentText()
-        if self.model.from_expression == QuantumExpression.MATRIX.name:
+        self.model.from_expression = QuantumExpression[
+            self.view.from_combo.currentText()
+        ]
+        if self.model.from_expression == QuantumExpression.MATRIX:
             self.view.set_to_combo_items([QuantumExpression.CIRCUIT.name])
         else:
             self.view.set_to_combo_items(expressions)
@@ -49,7 +50,12 @@ class ConverterPresenter:
         """
         update to_expression
         """
-        self.model.to_experssion = self.view.to_combo.currentText()
+        if len(self.view.to_combo.currentText()) == 0:
+            self.model.to_experssion = QuantumExpression.CIRCUIT
+        else:
+            self.model.to_experssion = QuantumExpression[
+                self.view.to_combo.currentText()
+            ]
 
     def on_value_name_changed(self) -> None:
         """
